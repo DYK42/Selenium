cc73e4153d96c0da0f24685676ac007dcd8e70c3
DYK42 <inkvizitor42@gmail.com>
2026-06-13 15:52:10 +0700

    add tests

---
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -0,0 +1,87 @@
+package ru.netology.web;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CallbackTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        //System.setProperty("webdriver.chrome.driver", "./drivers/win/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+//        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldPositiveTest() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Васильев Василий");
+        elements.get(1).sendKeys("+79270000000");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    @Test
+    void shouldNegativeNameTest() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Ivanov Ivan");
+        elements.get(1).sendKeys("+79270000000");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        List<WebElement> subscribes = driver.findElements(By.className("input__sub"));
+        String text = subscribes.getFirst().getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+
+    @Test
+    void shouldNegativePhoneTest() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Васильев Василий");
+        elements.get(1).sendKeys("927000000");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        List<WebElement> subscribes = driver.findElements(By.className("input__sub"));
+        String text = subscribes.get(1).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
+
+
+    //label .input_invalid
+}

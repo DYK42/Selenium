83309d9a03b7394e17c6108314a966330b08ad30
DYK42 <inkvizitor42@gmail.com>
2026-06-13 10:00:57 +0700

    customization .appVeyor.yml and build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,19 @@ plugins {
 group = "ru.netology"
 version = "1.0-SNAPSHOT"
 
+// используем 11 версию языка
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+// кодировка файлов (если используете русский язык в файлах)
+tasks {
+    withType<JavaCompile>().configureEach { options.encoding = "UTF-8" }
+    withType<JavaExec>().configureEach { defaultCharacterEncoding = "UTF-8" }
+    withType<Javadoc>().configureEach { options.encoding = "UTF-8" }
+    withType<Test>().configureEach { defaultCharacterEncoding = "UTF-8" }
+}
+
 repositories {
     mavenCentral()
 }
@@ -12,6 +25,8 @@ repositories {
 dependencies {
     implementation("org.seleniumhq.selenium:selenium-java:4.21.0")
     implementation("commons-io:commons-io:2.6")
+    // Source: https://mvnrepository.com/artifact/io.github.bonigarcia/webdrivermanager
+    implementation("io.github.bonigarcia:webdrivermanager:6.3.3")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
